193aadd182876dee4d9f60aa20058536f40fe9ff
Farid <faridun.pacan@mail.ru>
2021-07-06 04:42:31 +0300

    created nests for components

--- src/router.js
+++ src/router.js
@@ -1,18 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import coachDetail from './pages/coaches/coachDetail.vue';
+import coachesList from './pages/coaches/CoachesList.vue';
+import coachRegistration from './pages/coaches/CoachRegistration.vue';
+import contactCoach from './pages/requests/ContactCoach.vue';
+import requestRecieved from './pages/requests/RequestRecieved.vue';
+import coachDetail from './pages/NotFound.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', redirect: '/coaches' },
-        { path: '/coaches', componen: null },
+        { path: '/coaches', componen: coachesList },
         {
             path: '/coaches/:id',
-            componen: null,
-            children: [{ path: 'contact', component: null }]
+            componen: coachDetail,
+            children: [{ path: 'contact', component: contactCoach }]
         },
-        { path: '/register', componen: null },
-        { path: '/requests', componen: null },
-        { path: '/:notFound(.*)', componen: null }
+        { path: '/register', componen: coachRegistration },
+        { path: '/requests', componen: requestRecieved },
+        { path: '/:notFound(.*)', componen: coachDetail }
     ]
 });
 
