b1771d9a13e62fc58af9bfdc8e0961661820b8fe
Farid <faridun.pacan@mail.ru>
2021-07-08 15:04:54 +0300

    created Vuex structure

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -2,4 +2,21 @@
     <section>
         COACH LIST
     </section>
+    <ul v-if="hasCoaches">
+        <li v-for="coach in filteredCoaches" :key="coach.id">{{coach.firstName}}</li>
+    </ul>
+    <p v-else>No coaches Found!</p>
 </template>
+
+<script>
+export default {
+    computed: {
+        filteredCoaches () {
+            return this.$store.getters['coaches/coaches'];
+        },
+        hasCoaches() {
+            return this.$store.getters['coaches/hasCoaches'];
+        }
+    }
+}
+</script>
