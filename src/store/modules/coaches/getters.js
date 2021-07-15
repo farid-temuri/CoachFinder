5c8f4aba36dd017cd30c86a92a3dc39527755f7b
Farid <faridun.pacan@mail.ru>
2021-07-15 15:46:16 +0300

    fixed Coach registration button (now disappears correctly)

--- src/store/modules/coaches/getters.js
+++ src/store/modules/coaches/getters.js
@@ -8,6 +8,6 @@ export default {
     isCoach(_state, getters, _rootState, rootGetters) {
         const coaches = getters.coaches;
         const userId = rootGetters.userId;
-        return coaches.some(coach => { coach.id === userId });
+        return coaches.some(coach => coach.id == userId);
     }
 }
