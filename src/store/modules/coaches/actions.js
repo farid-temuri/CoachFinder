ae580754753ede6b5d223800a4140573cd1525f2
Farid <faridun.pacan@mail.ru>
2021-07-16 03:18:49 +0300

    put request on register coach added

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,13 +1,29 @@
 export default {
-    registerCoach(context, data) {
-        const coahData = {
-            id: context.rootGetters.userId,
+    async registerCoach(context, data) {
+        const userId = context.rootGetters.userId;
+        const coachData = {
             firstName: data.first,
             lastName: data.last,
             areas: data.areas,
             description: data.desc,
-            hourlyRate: data.rate,
+            hourlyRate: data.rate
         };
-        context.commit('registerCoach', coahData);
+
+        const response = await fetch(
+            'https://axaxa-9ce0c-default-rtdb.firebaseio.com/coaches/' +
+            userId +
+            '.json', {
+                method: 'PUT',
+                body: JSON.stringify(coachData)
+            }
+        );
+        // const responseData = await response.json();
+        if (!response.ok) {
+            console.log(response);
+        }
+        context.commit('registerCoach', {
+            ...coachData,
+            id: userId
+        });
     }
 };
